53bf71747bc95e7e1dc1bbded0a6f35098964cdf
Greencho <Greencho@DESKTOP-0KIIREV>
2019-08-20 22:14:09 -0500

    Testing Spring

---
+++ Practica3/src/com/practica/HolaController.java
@@ -0,0 +1,19 @@
+package com.practica;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HolaController {
+
+	@RequestMapping("irHola")
+	public ModelAndView redirection() 
+	{
+		ModelAndView MV = new ModelAndView("Hola", "mensaje", "miau");
+		
+	
+		
+		return MV;
+	}
+}
